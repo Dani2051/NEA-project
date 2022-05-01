f670b9ca7ca96c6d84934f48afbfefa33467a13d
Dani2051 <dardaniyaal@gmail.com>
2022-05-02 00:25:04 +0100

    homepage PHP file

---
+++ website/home/homePage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="stylesheet" href="homeCSS.css">
+
+<h1>Home</h1>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<form action="..\login\loginPage.php" method="POST">
+    <input type="submit" value="Login"><br>
+</form>
+<form action="..\register\registerPage.php" method="POST">
+    <input type="submit" value="Register"><br>
+</form>
+
+</head>
+
+<body>
+    
+</body>
+
+
+</html>
