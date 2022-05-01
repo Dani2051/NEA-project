a3bab9a07482aaec0a5d5fa9fbc8d509b78598a9
Dani2051 <dardaniyaal@gmail.com>
2022-05-02 00:25:47 +0100

    Create registerPage.php

---
+++ website/register/registerPage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<link rel="stylesheet" href="registerCSS.css">
+
+<h1>Register</h1>
+
+<style>
+    
+</style>
+
+<form action="registerProcess.php" method="POST">
+    <label for="uname">Enter username: </label><br>
+    <input type="text" id="uname" name="uname" ><br>
+    <label for="pword">Enter password: </label><br>
+    <input type="text" id="pword" name="pword" ><br><br>
+    <input type="submit" value="Register"><br>
+</form>
+
+</head>
+
+<body>
+
+<li><a href="../login/loginPage.php">login</a></li>
+    
+</body>
+
+
+</html>
