d2d46d0713289c5fd8097d9b31c9903b47325d8e
Dani2051 <dardaniyaal@gmail.com>
2022-05-15 16:21:57 +0100

    Update registerPage.php

--- website/register/registerPage.php
+++ website/register/registerPage.php
@@ -1,32 +1,78 @@
 <!DOCTYPE html>
-
 <html>
+
+
 
 <head>
 
-<link rel="stylesheet" href="registerCSS.css">
-
-<h1>Register</h1>
+<link rel="stylesheet" href="../websiteCSS.css"> 
 
 <style>
-    
-</style>
+ 
+body {
+    font-family: Arial, Helvetica, sans-serif;
+}
 
-<form action="registerProcess.php" method="POST">
-    <label for="uname">Enter username: </label><br>
-    <input type="text" id="uname" name="uname" ><br>
-    <label for="pword">Enter password: </label><br>
-    <input type="text" id="pword" name="pword" ><br><br>
-    <input type="submit" value="Register"><br>
-</form>
+.form {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    box-sizing: border-box;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    position: relative;
+    left: 0%;
+    padding: 12px 20px;
+    margin: 0px 0;
+    display: inline-block;
+    border: 1px solid rgb(0, 0, 0);
+    background-color: #e8f0fe;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+
+#p {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+}
+
+#button {
+    width: 100%;
+    cursor: pointer;
+    margin: 10px 0;
+}
+
+</style>
 
 </head>
 
 <body>
+   
+<div id="p">
+<p>
+    Already have an account? <a href="..\login\loginPage.php">Sign in</a>
+</p>
+</div>
 
-<li><a href="../login/loginPage.php">login</a></li>
-    
+<div class="form">
+    <form action="registerProcess.php" method="POST">
+        <input type="text" name="uname" placeholder="Enter Username" required><br>
+        <input type="password" name="pword" placeholder="Enter Password" required><br>
+        <input id="button" type="submit" value="Register">
+    </form>
+</div>
+
 </body>
+
+
 
 
 </html>
