c9929aff01d143ea3e0b1d221fc1fd8e30111e5c
Dani2051 <dardaniyaal@gmail.com>
2022-05-24 09:19:47 +0100

    Create registerProcess.php

---
+++ website/login/registerProcess.php
@@ -0,0 +1,40 @@
+<?php
+    $servername = "localhost";
+    $username = "ddar";
+    $password = "tjwa1234";
+    $dbname = "DungeonDB";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+$un = $_POST['uname'];
+$pw = password_hash($_POST['pword'], PASSWORD_DEFAULT);
+
+// cleanse input htmlspecialchars < &_234
+
+    //check to make sure username is unique 
+    if ($un == "" or $_POST['pword'] == "") {
+        header('location: https://2-12.co.uk/~ddar/NEA-project/website/home/homePage.php');
+        $_SESSION['allFields'] = "false";
+        
+    } else {
+        $sql = "SELECT * FROM loginData WHERE usernames = '$un'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result->num_rows == 1) {
+            echo "username already exists";
+        } else {
+            //USERNAME AVAILABLE
+            $sql = "INSERT into loginData (usernames, passwords) VALUES ('$un','$pw')";
+            $result = mysqli_query($conn, $sql);
+
+            echo "Your account has sucessfully been created!";
+        }
+    }
+
+    mysqli_close($conn);
+?>
